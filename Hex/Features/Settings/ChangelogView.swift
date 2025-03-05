dd36fb9e25686e80c220968429002bea1f20fe8c
luksab <lukas@sabatschus.de>
2025-03-05 21:06:04 +0100

    Add button to show changelog

---
+++ Hex/Features/Settings/ChangelogView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+import MarkdownUI
+
+struct ChangelogView: View {
+    @Environment(\.dismiss) var dismiss
+
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Changelog")
+                    .font(.title)
+                    .padding(.bottom, 10)
+
+                if let changelogPath = Bundle.main.path(forResource: "changelog", ofType: "md"),
+                    let changelogContent = try? String(
+                        contentsOfFile: changelogPath, encoding: .utf8)
+                {
+                    Markdown(changelogContent)
+                } else {
+                    Text("Changelog could not be loaded.")
+                        .foregroundColor(.red)
+                }
+
+                Spacer()
+
+                Button("Close") {
+                    dismiss()
+                }
+                .buttonStyle(.borderedProminent)
+                .padding(.top, 20)
+            }
+            .padding()
+        }
+    }
+}
